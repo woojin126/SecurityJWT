bd4f7abc868a0b9ffc22f066bedd471a49ff49cd
woojin126 <woojin126@naver.com>
2021-08-15 21:04:02 +0900

    스프링시큐리티 필터가아닌 내가 만든 필터 ( 스프링시큐리티 필터 동작전에 실행됨 @Request요청시바로)

---
+++ src/main/java/com/cos/jwt/config/FilterConfig.java
@@ -0,0 +1,30 @@
+package com.cos.jwt.config;
+
+import com.cos.jwt.filter.MyFilter1;
+import com.cos.jwt.filter.MyFilter2;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 직접 만든필터는 request요청이 올때 바로 실행 (시큐리티 필터 전에 실행되도록 SecuriryConfig에 설정함
+ */
+@Configuration//메모리에뜨는작업
+public class FilterConfig { //스프링시큐리티 필터에 체인을 거는게아닌 내가따로 만든 필터
+
+    @Bean
+    public FilterRegistrationBean<MyFilter1> filter1(){
+        FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
+        bean.addUrlPatterns("/*");
+        bean.setOrder(0);//낮은번호가 필터중에서 가장 먼저 실행됨
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<MyFilter2> filter2(){
+        FilterRegistrationBean<MyFilter2> bean = new FilterRegistrationBean<>(new MyFilter2());
+        bean.addUrlPatterns("/*");
+        bean.setOrder(1);//낮은번호가 필터중에서 가장 먼저 실행됨
+        return bean;
+    }
+}
